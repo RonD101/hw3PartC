f4d42a6c20c18369da8b6a8ea7709188e874862d
rond <ron.dahan@campus.technion.ac.il>
2020-06-22 22:00:48 +0300

    part c motherfucker!

---
+++ cmake-build-debug/CMakeFiles/hw3PartC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw3PartC.dir/main.cpp.o"
+  "hw3PartC"
+  "hw3PartC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw3PartC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
