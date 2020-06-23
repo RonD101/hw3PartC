85748a7976e761acff1f0212dca873c1088cbc0b
yoav <kenkendler123@gmail.com>
2020-06-23 22:54:06 +0300

    catch this

--- Medic.cpp
+++ Medic.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Medic.h"
+#define AMMO_RELOAD 5
+
+int mtm::Medic::reload() const {
+    return AMMO_RELOAD;
+}
+
+std::shared_ptr<mtm::Character> mtm::Medic::clone() const {
+    return std::shared_ptr<Character>(new Medic(*this));
+}
--- Medic.h
+++ Medic.h
@@ -5,4 +5,14 @@
 #ifndef HW3PARTC_MEDIC_H
 #define HW3PARTC_MEDIC_H
 
+#include "Character.h"
+#include <memory>
+
+namespace mtm{
+class Medic: public Character{
+    public:
+        std::shared_ptr<Character> clone() const override;
+        virtual int reload() const override;
+    };
+}
 #endif //HW3PARTC_MEDIC_H
