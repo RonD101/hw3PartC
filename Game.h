01e4fc7a18c8c8494791ab9fcb6c84b5e8dbda1c
yoav <kenkendler123@gmail.com>
2020-06-29 10:58:45 +0300

    catch this

--- Game.h
+++ Game.h
@@ -8,6 +8,27 @@
 #include "Character.h"
 #include "Matrix.h"
 
+/**
+* Implements class Game under namespace mtm.
+* Uses Matrix<std::shared_ptr<Character>> from Matrix.h to
+* implement the board.
+*
+* The following functions are available:
+*   constructor and copy constructor.
+*   getCharacter    - Return smart pointer for the contents of the board in the coordinates param.
+*   distance		- Return an int of the distance between src_coordinates and dst_coordinates.
+*   clearDeads		- Removes all characters on the board with health <= 0.
+*   addCharacter	- Adds an existing character to the game.
+*   makeCharacter   - Creates a new character with all the required character parameters
+*   move  	        - Moves a character in the game from src_coordinates to dst_coordinates
+*   attack		    - Makes character in src_coordinates attack character in dst_coordinates
+*   isOver	        - Return true the game has been won, return false if not.
+*
+* The following operators are available:
+*   operator=      - Assignment of one game to the other
+*   operator<<     - Return an ostream value to print
+*/
+
 namespace mtm{
     class Game{
         Matrix<std::shared_ptr<Character>> board;
@@ -18,6 +39,14 @@ namespace mtm{
     public:
         Game(int height, int width);
         Game(const Game& game);
+
+        /**	operator=: assign game
+        *
+        * @param
+        * other - The matrix which we copy the values from.
+        * @return
+        * 	The assigned matrix
+        */
         Game &operator=(const Game& other);
         void addCharacter(const GridPoint& coordinates, std::shared_ptr<Character> character);
         static std::shared_ptr<Character> makeCharacter(CharacterType type, Team team,
